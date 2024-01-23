7cad9e4f2300e14ab0d46ba10be2cf82d37f88ce
navnforfun@gmail.com <navnforfun@gmail.com>
2024-01-23 09:42:55 +0700

    Tuan 4

---
+++ ex2_Stream/src/ex2_stream/Ex2_Stream.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author ngocanh
+ */
+public class Ex2_Stream {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Stream");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap vao bien x = ");
+        int x = sc.nextInt();
+        System.out.println("x = " + x);
+    }
+    
+}
